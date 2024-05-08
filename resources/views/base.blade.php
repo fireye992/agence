4ad09874b236f48885586eb97cd19b5ca9b3e974
Fenrir <fireye@wanadoo.fr>
2024-05-08 16:34:39 +0200

    jet3

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+{{-- attention y a aussi un admin.blade pour les  fonction admin avec un header different --}}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://unpkg.com/alpinejs" defer></script>
 
     @livewireStyles
 
